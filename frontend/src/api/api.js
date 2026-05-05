1a13ed2fae2787462c0610306b58ff7964c06c6f
pratik parmar <pratikparmar0502@gmail.com>
2026-05-05 16:49:28 +0530

    render url changed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+console.log("Connecting to:", import.meta.env.VITE_API_URL);
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const authServices = {
-  login: (data) => API.post("/users/login", data),
   register: (data) => API.post("/users/register", data),
+  login: (data) => API.post("/users/login", data),
 };
